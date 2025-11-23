47e99a0019f119af634ba44b0a0d5cf7c6ee3674
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-23 08:15:53 +0530

    Updated Admin Controller

--- suvarnadesk-backend/src/controllers/adminController.ts
+++ suvarnadesk-backend/src/controllers/adminController.ts
@@ -1,17 +1,19 @@
-import Admin from '../models/Admin';
-import { hashPassword, comparePassword } from '../utils/hashPassword';
-import jwt from 'jsonwebtoken';
-import { Request, Response } from 'express';
+import Admin from "../models/Admin";
+import { hashPassword, comparePassword } from "../utils/hashPassword";
+import jwt from "jsonwebtoken";
+import { Request, Response } from "express";
 
 export const loginAdmin = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const admin = await Admin.findOne({ email });
-    if (!admin) return res.status(401).json({ message: 'Invalid credentials' });
-
+    if (!admin) return res.status(401).json({ message: "Invalid credentials" });
     const valid = await comparePassword(password, admin.password);
-    if (!valid) return res.status(401).json({ message: 'Invalid credentials' });
-
-    const token = jwt.sign({ adminId: admin._id, role: admin.role }, process.env.JWT_SECRET || '', { expiresIn: '12h' });
+    if (!valid) return res.status(401).json({ message: "Invalid credentials" });
+    const token = jwt.sign(
+        { adminId: admin.id, role: admin.role },
+        process.env.JWT_SECRET!,
+        { expiresIn: "12h" }
+    );
     res.json({ token, admin });
 };
 
