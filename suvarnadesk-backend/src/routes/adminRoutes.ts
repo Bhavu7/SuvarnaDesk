8605fb557323c84ba1523a54a939196a04e370a9
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-23 08:16:20 +0530

    Updated Admin Routes

--- suvarnadesk-backend/src/routes/adminRoutes.ts
+++ suvarnadesk-backend/src/routes/adminRoutes.ts
@@ -1,6 +1,8 @@
-import { Router } from 'express';
-import { loginAdmin, registerAdmin } from '../controllers/adminController';
+import { Router } from "express";
+import { loginAdmin, registerAdmin } from "../controllers/adminController";
+
 const router = Router();
-router.post('/login', loginAdmin);
-router.post('/register', registerAdmin);
+router.post("/login", loginAdmin);
+router.post("/register", registerAdmin);
+
 export default router;
