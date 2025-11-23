b620d1498cc47f5536b66b36f687b2dc3a0158e4
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-23 11:07:47 +0530

    Updated Invoice QR Code UI

--- suvarnadesk-frontend/src/components/InvoiceQRCode.tsx
+++ suvarnadesk-frontend/src/components/InvoiceQRCode.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import { QRCodeSVG } from "qrcode.react";
 
-export default function InvoiceQRCode({ data }: { data: string }) {
-  return (
-    <div className="p-2 border rounded">
-      <QRCodeSVG value={data} size={150} />
-    </div>
-  );
-}
+const InvoiceQRCode = ({ data }: { data: string }) => (
+  <div className="inline-block p-2 my-2 bg-white border border-gray-200 rounded">
+    <QRCodeSVG value={data} size={150} />
+  </div>
+);
+
+export default InvoiceQRCode;
