eefe8eb62dc1a74498768defb5220af7fa2d27cb
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-23 07:52:25 +0530

    Added Custom Font

--- suvarnadesk-frontend/tailwind.config.js
+++ suvarnadesk-frontend/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      inter: ["Poppins", "sans-serif"], // or your chosen font
+    },
   },
   plugins: [],
 };
