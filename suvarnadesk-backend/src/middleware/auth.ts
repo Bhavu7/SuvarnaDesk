38448869675bf9eb0ff00294765f6dcbaa438704
Bhavesh Bhoi <bhavubhoi806@gmail.com>
2025-11-23 08:18:22 +0530

    Updated Middleware Auth

--- suvarnadesk-backend/src/middleware/auth.ts
+++ suvarnadesk-backend/src/middleware/auth.ts
@@ -1,14 +1,15 @@
-import jwt from 'jsonwebtoken';
-import { Request, Response, NextFunction } from 'express';
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
 
+// Protect routes
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'No token provided.' });
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ message: "No token provided." });
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!);
         (req as any).admin = decoded;
         next();
     } catch {
-        return res.status(401).json({ message: 'Invalid token.' });
+        return res.status(401).json({ message: "Invalid token." });
     }
 };
